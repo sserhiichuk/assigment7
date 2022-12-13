258d9c7dfadc06b4a36ec389981cace04131b859
Roman Radiiovskyi <radiiovskyi@gmail.com>
2022-12-13 19:05:21 +0300

    did task2 and task3 but task3 need some fix

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def main():
 
     if args.all == "medals":
         taks1(args.filename, args.country, args.year, args.noc, args.output)
+    elif args.all == "total":
+        task2(args.filename, args.year)
+    elif args.all == "overall":
+        task3(args.filename, args.country)
 
 
 def taks1(filename, country, year, noc, output):
@@ -52,3 +56,49 @@ def taks1(filename, country, year, noc, output):
         if output:
             with open("new_file", "w") as file:
                 file.write(f"{result_list}\n {how_many_medals}")
+
+
+def task2(filename, year):
+    with open(filename, "r") as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            data = line.strip().split("\t")
+            if data[9] == year:
+                if data[14] != 'NA':
+                    result = f'{data[6]} - {data[14]}'
+                    print(result)
+
+
+def task3(filename, country):
+    every_country = country.split(',')
+    print(every_country)
+    idx = 0
+    current_country = every_country[idx]
+    gold = 0
+    silver = 0
+    bronze = 0
+
+    print(every_country[idx])
+    with open(filename, "r") as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            data = line.strip().split("\t")
+            if current_country == data[6]:
+                for current_country in line:
+                    if data[14] != "NA":
+                        if data[14] == 'Gold':
+                            gold += 1
+                        elif data[14] == 'Silver':
+                            silver += 1
+                        elif data[14] == 'Bronze':
+                            bronze += 1
+                result = f'{current_country} - {gold} - {silver} - {bronze}\n'
+                idx += 1
+                print(result)
+
+
+main()
